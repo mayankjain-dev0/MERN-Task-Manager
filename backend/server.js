7831bbc33ed13ffc190690d03dcfac3d323ffa32
mayank-jain-coder <mayankjain9331@gmail.com>
2026-04-07 04:04:48 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error(err));
 
 app.get("/", (req, res) => res.send("API running"));
+app.get("/healthz", (req, res) => {
+  res.status(200).send("Server is healthy");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
