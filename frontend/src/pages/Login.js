4c05bcb5677d2c67521a4348ac8e09b7375be405
mayank-jain-coder <mayankjain9331@gmail.com>
2026-04-13 19:13:22 +0530

    updated login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await axios.post("http://localhost:5000/api/users/login", { email, password });
+    const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/users/login`, { email, password });
     localStorage.setItem("token", res.data.token);
     alert("Logged In!!");
   };
